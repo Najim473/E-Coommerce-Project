f1bf2be9ce7cda129bad31135effef3dccd07def
najim473 <najim7304@gmail.com>
2022-08-06 11:34:36 +0600

    created password

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 const app = express();
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter)
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
